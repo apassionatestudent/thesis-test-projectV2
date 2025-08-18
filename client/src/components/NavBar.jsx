1b7d5839cf2f80bbe2da91ca5668de677d2121f9
bert114 <janalbertpenano14@gmail.com>
2025-08-18 11:38:04 +0800

    try

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,15 +1,26 @@
 // src/components/Navbar.jsx
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
+// 6pm-8 task
 function Navbar() {
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/aboutus">About Us</Link></li>
-        <li><Link to="/contactus">Contact Us</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/login">Login</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/aboutus">About Us</Link>
+        </li>
+        <li>
+          <Link to="/contactus">Contact Us</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
       </ul>
     </nav>
   );
